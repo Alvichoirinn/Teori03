c9f1317c854a5d5a6447d2c77975e472f6544d81
alvichoirinn <avialvi08@gmail.com>
2024-02-28 15:18:37 +0700

    Teori03

---
+++ CateenDemo.java
@@ -0,0 +1,22 @@
+public class CateenDemo {
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        item1.name = "Nasi Campur";
+        item1.price = 10000;
+        item1.stock = 10;
+        item1.displayInfo();
+
+        Item item2 = new Item();
+        item2.name = "Nasi Padang";
+        item2.price = 15000;
+        item2.stock = 15;
+        item2.displayInfo();
+
+        Item item3 = new Item();
+        item3.name = "Es teh";
+        item3.price = 4000;
+        item3.stock = 10;
+        item2.displayInfo();
+    }
+}
+    
---
+++ Item.java
@@ -0,0 +1,15 @@
+/**
+ * Item
+ */
+public class Item {
+
+    public String name;
+    public int price;
+    public int stock;
+
+    public void displayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Stock: " + stock);
+    }
+}
